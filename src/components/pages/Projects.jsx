18152acc15d1493ae4d92d3cf19c9d230f1985f4
Yash Srivastava <113162308+yashsrivastavaaa@users.noreply.github.com>
2025-08-25 12:40:28 +0530

    Update Projects.jsx

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -29,7 +29,7 @@ const Projects = () => {
         </motion.div>
 
         {/* Projects Grid */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {projects.map((project) => (
             <motion.div
               key={project.id}
