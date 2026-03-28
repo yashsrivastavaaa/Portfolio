3de142324d157b1aad72807107be3c54f80f3cf6
Yash Srivastava <113162308+yashsrivastavaaa@users.noreply.github.com>
2026-03-28 15:49:54 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import About from "./components/pages/About";
 import Skills from "./components/pages/Skills";
 import Contact from "./components/pages/Contact";
 import Projects from "./components/pages/Projects";
+import Experience from "./components/pages/Experience";
 
 
 const App = () => {
@@ -17,6 +18,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/experience" element={<Experience />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/skills" element={<Skills />} />
           <Route path="/contact" element={<Contact />} />
